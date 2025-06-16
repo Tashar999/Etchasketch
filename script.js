81415066d337f7fae8b4f16648568b5ee9e62bf4
Tashar999 <27tashar@cpsd.us>
2025-06-15 23:39:31 -0400

    created button corresponding to function to make cutom new grids

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const container = document.querySelector('.container');
+const btn = document.getElementById('btn')
+num = 0
 
-for (let i = 0; i < 256; i++) {
-const div = document.createElement('div');
-container.appendChild(div); 
-}
-
+function grid(){
+    container.innerHTML = "";
+    let num = prompt('How many squares do you want the grid to be?');
+    if (num >= 100 || num <= 0){
+        alert('gotta be 1-100 man')
+    } else {
+    for (let i = 0; i < num * num; i++) {
+    const div = document.createElement('div');
+    container.appendChild(div);
+    div.style.width = 960/num + "px"
+    div.style.height = 960/num + "px"
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = "red";
+            })}
+}}
